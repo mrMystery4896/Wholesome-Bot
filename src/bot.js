f78501f59a676ceb79a45d879c6e8b6b4923f835
KS <kaokaisiang021001@gmail.com>
2021-12-14 08:38:42 +0800

    Added github and help command.

--- src/bot.js
+++ src/bot.js
@@ -8,31 +8,38 @@ const PREFIX = "!";
 let normalChannel;
 let nsfwChannel;
 
-let scheduledMessage = new cron.CronJob("01 05 01,13 * * *", async () => {
-	if (normalChannel) {
-		let attachment;
-		let channel;
-		let result = await getRandomImage();
-		if (result === "error") return message.channel.send("error");
-		//if result is nsfw and there is no nsfw channel, repeat the loop to attempt to find a non-nsfw image
-		while (result.nsfw && !nsfwChannel) {
-			result = await getRandomImage();
-			console.log(result.nsfw);
-			console.log(nsfwChannel);
+let scheduledMessage = new cron.CronJob(
+	"0 05 10,22 * * *",
+	async () => {
+		if (normalChannel) {
+			console.log("Running function...");
+			let attachment;
+			let channel;
+			let result = await getRandomImage();
+			if (result === "error") return message.channel.send("error");
+			//if result is nsfw and there is no nsfw channel, repeat the loop to attempt to find a non-nsfw image
+			while (result.nsfw && !nsfwChannel) {
+				result = await getRandomImage();
+				console.log(result.nsfw);
+				console.log(nsfwChannel);
+			}
+			if (result.nsfw) {
+				channel = await client.channels.fetch(nsfwChannel);
+			} else {
+				channel = await client.channels.fetch(normalChannel);
+			}
+			channel.send("Here is your daily dose your wholesomeness ~UwU~");
+			result.images.forEach((img) => {
+				const sfbuff = new Buffer.from(img, "base64");
+				attachment = new MessageAttachment(sfbuff, "image.jpg");
+				channel.send(attachment);
+			});
 		}
-		if (result.nsfw) {
-			channel = await client.channels.fetch(nsfwChannel);
-		} else {
-			channel = await client.channels.fetch(normalChannel);
-		}
-		channel.send("Here is your daily dose your wholesomeness ~UwU~");
-		result.images.forEach((img) => {
-			const sfbuff = new Buffer.from(img, "base64");
-			attachment = new MessageAttachment(sfbuff, "image.jpg");
-			channel.send(attachment);
-		});
-	}
-});
+	},
+	null,
+	false,
+	"Asia/Kuala_Lumpur"
+);
 scheduledMessage.start();
 
 client.on("message", async (message) => {
@@ -58,8 +65,23 @@ client.on("message", async (message) => {
 				break;
 			}
 
-			default:
+			case "github": {
+				message.channel.send(
+					"Here's a link to the GitHub repository: https://github.com/mrMystery4896/Wholesome-Bot"
+				);
 				break;
+			}
+
+			case "help": {
+				message.channel.send(
+					"!set (normal/nsfw): set the current channel to post images here. Nsfw pictures will be posted to nsfw channel.\n!github: get the GitHub link to the repository"
+				);
+			}
+
+			default: {
+				message.channel.send("This is an invalid command!");
+				break;
+			}
 		}
 	}
 });
